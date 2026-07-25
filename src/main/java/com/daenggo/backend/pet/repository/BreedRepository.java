4c56657cd396c30354fe962b4a43c0f07cf484b3
Seok_Soo <whdtn7855@naver.com>
2026-07-25 16:18:10 +0900

    feat: 견종 선택 기능 추가 - 견종 목록 넣고 선택할 수 있도록 구현 - 견종명, 맹견 여부로 정렬

--- src/main/java/com/daenggo/backend/pet/repository/BreedRepository.java
+++ src/main/java/com/daenggo/backend/pet/repository/BreedRepository.java
@@ -3,9 +3,12 @@ package com.daenggo.backend.pet.repository;
 import com.daenggo.backend.pet.entity.Breed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BreedRepository extends JpaRepository<Breed, Long> {
 
     Optional<Breed> findByName(String name);
+
+    List<Breed> findAllByNameNotOrderByDangerousAscNameAsc(String excludedName);
 }
