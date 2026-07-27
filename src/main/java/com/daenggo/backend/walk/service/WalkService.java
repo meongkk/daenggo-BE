dfb86c7a2f91342c6646078f714fd8e449e13620
alsthdua107 <alsthdua107@gmail.com>
2026-07-27 16:51:55 +0900

    fix: 1km 미만 산책 시에는 페이스 출력 x

--- src/main/java/com/daenggo/backend/walk/service/WalkService.java
+++ src/main/java/com/daenggo/backend/walk/service/WalkService.java
@@ -117,7 +117,7 @@ public class WalkService {
 		// 평균 페이스 계산 (단위 : sec)
 		int avgPaceSec = 0;
 		if (request.getDistanceM() != null 
-				&& request.getDistanceM().doubleValue() > 0) {
+				&& request.getDistanceM().doubleValue() > 1) {
 			
 	        avgPaceSec =
 	        		(int) (durationSec / (request.getDistanceM().doubleValue() / 1000.0));
